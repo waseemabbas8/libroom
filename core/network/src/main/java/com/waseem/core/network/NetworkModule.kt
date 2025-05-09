50d65fa603833ab9974c29f413c8b793bfb714e8
Waseem Abbas <waseemabbas.dev@gmail.com>
2025-05-09 18:19:40 +0500

    setup the network layer di

--- core/network/src/main/java/com/waseem/core/network/NetworkModule.kt
+++ core/network/src/main/java/com/waseem/core/network/NetworkModule.kt
@@ -1,5 +1,22 @@
 package com.waseem.core.network
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
+@Module
+@InstallIn(SingletonComponent::class)
 internal object NetworkModule {
+
+    @Provides
+    fun provideServer(): Server {
+        //TODO: Get server based on product flavor
+        return Server.Staging
+    }
+
+    @Provides
+    fun provideNetworkClient(server: Server): NetworkClient {
+        return NetworkClient(server)
+    }
 }
